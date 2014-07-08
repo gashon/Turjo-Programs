0e28715e4054634e25e931341033bb883529af34
gashon <nht570@gmail.com>
2014-07-08 02:40:16 -0700

    Create sum of two float numbers.c

---
+++ sum of two float numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("Enter the first floating number: ");
+    scanf("%f",&a);
+    printf("Enter the second floating number: ");
+    scanf("%f",&b);
+    printf("sum = %f",a+b);
+}
