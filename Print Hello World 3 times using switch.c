2bb7580b9df9b82b3eff1774dcd9e1a1ddfbfff0
gashon <nht570@gmail.com>
2014-08-18 12:25:13 +0600

    Create Print Hello World 3 times using switch.c

---
+++ Print Hello World 3 times using switch.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 3: printf("Hello World\n");
+        case 2: printf("Hello World\n");
+        case 1: printf("Hello World");
+                break;
+    }
+    return 0;
+}
